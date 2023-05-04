f6de584ec38af51d0a7751d3d40f724bfedcaf4c
sooongg <122096878+jisong-Shin@users.noreply.github.com>
2023-05-04 17:44:39 +0900

    230504 댓글 기능추가

--- src/main/java/com/spring/mvc/chap05/api/ReplyController.java
+++ src/main/java/com/spring/mvc/chap05/api/ReplyController.java
@@ -1,18 +1,16 @@
 package com.spring.mvc.chap05.api;
 
 import com.spring.mvc.chap05.dto.ReplyListResponseDTO;
+import com.spring.mvc.chap05.dto.ReplyModifyRequestDTO;
+import com.spring.mvc.chap05.dto.ReplyPostRequestDTO;
 import com.spring.mvc.chap05.dto.page.Page;
-import com.spring.mvc.chap05.entity.Reply;
 import com.spring.mvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.ResponseEntity.*;
 
@@ -20,6 +18,8 @@ import static org.springframework.http.ResponseEntity.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/replies")
 @Slf4j
+// 클라이언트의 접근을 어떤 app에서만 허용할 것인가?
+@CrossOrigin(origins = {"http://127.0.0.1:5500"})
 public class ReplyController {
 
     private final ReplyService replyService;
@@ -40,5 +40,83 @@ public class ReplyController {
         ReplyListResponseDTO replyList = replyService.getList(boardNo, page);
 
         return ok().body(replyList);
+    }
+
+
+    // 댓글 등록 요청
+    @PostMapping// 뒤에() 내용 안쓰면 requestmapping() 내용과 동일하게 인식됨
+    public ResponseEntity<?> create(
+            // 요청 메시지 바디에 JSON으로 보내주세요
+            @Validated @RequestBody ReplyPostRequestDTO dto
+            // validated는 dto에 건 내용들을 검증함
+            , BindingResult result // 검증결과를 가진 객체
+    ) {
+        // 입력값 검증에 걸리면 4xx 상태코드 리턴
+        if (result.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.toString());
+        }
+        log.info("/api/v1/replies : POST! ");
+        log.info("param: {} ", dto);
+
+        // 서비스에 비즈니스 로직 처리 위임
+        try {
+            ReplyListResponseDTO responseDTO = replyService.register(dto);
+            // 성공시 클라이언트에 응답하기
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            // 문제발생 상황을 클라이언트에게 전달
+            log.warn("500 Status code response!! caused by: {}", e.getMessage());
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage());
+        }
+    }
+
+    // 댓글 삭제 요청
+    @DeleteMapping("/{replyNo}")
+    public ResponseEntity<?> remove(
+            @PathVariable("replyNo") Long replyNo //  @PathVariable("replyNo")가 위 매핑의 주소와 같을땐 안써도 됨
+    ) {
+        if (replyNo == null) {
+            return ResponseEntity.badRequest().body("댓글 번호를 보내주세용꼬리용용🧜🏻‍♂️");
+        }
+        log.info("/api/v1/replies/{} : DELETE! ", replyNo);
+        try {
+            ReplyListResponseDTO responseDTO = replyService.delete(replyNo);
+            return ResponseEntity
+                    .ok()
+                    .body(responseDTO);
+
+        } catch (Exception e) {
+            return ResponseEntity
+                    .internalServerError()
+                    .body(e.getMessage());
+        }
+    }
+    // 댓글 수정 요청
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.PATCH})
+    public ResponseEntity<?> modify(
+            @Validated @RequestBody ReplyModifyRequestDTO dto
+            , BindingResult result
+    ) {
+
+        if (result.hasErrors()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(result.toString());
+        }
+
+        log.info("/api/v1/replies PUT!");
+        try {
+            ReplyListResponseDTO responseDTO = replyService.modify(dto);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            log.warn("500 status code! caused by: {}", e.getMessage());
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
 }
-}
+
--- src/main/java/com/spring/mvc/chap05/service/ReplyService.java
+++ src/main/java/com/spring/mvc/chap05/service/ReplyService.java
@@ -2,15 +2,22 @@ package com.spring.mvc.chap05.service;
 
 import com.spring.mvc.chap05.dto.ReplyDetailResponseDTO;
 import com.spring.mvc.chap05.dto.ReplyListResponseDTO;
+import com.spring.mvc.chap05.dto.ReplyModifyRequestDTO;
+import com.spring.mvc.chap05.dto.ReplyPostRequestDTO;
 import com.spring.mvc.chap05.dto.page.Page;
 import com.spring.mvc.chap05.dto.page.PageMaker;
 import com.spring.mvc.chap05.entity.Reply;
 import com.spring.mvc.chap05.repository.ReplyMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,5 +43,43 @@ public class ReplyService {
                 .replies(replies)
                 .build();
     }
+
+    // 댓글 둥록 서비스
+    public ReplyListResponseDTO register(final ReplyPostRequestDTO dto) // 매개변수에 final은 매개변수가 수정안되게 안전하게 지킴
+            throws SQLException {
+        log.debug("register service execute~!!");
+
+        // dto를 entity로 변환
+        Reply reply = dto.toEntity();
+        boolean flag = replyMapper.save(reply);
+
+        // 예외처리
+        if (!flag) {
+            log.warn("reply registered fail!!");
+            throw new SQLException("댓글 저장 실패!");
+        }
+        return getList(dto.getBno(), new Page(1, 10));
+    }
+
+    // 댓글 삭제 서비스
+    @Transactional // 트랜잭션 처리
+    public ReplyListResponseDTO delete(final long replyNo)
+            throws Exception {
+        long boardNo = replyMapper.findOne(replyNo).getBoardNo();
+        replyMapper.deleteOne(replyNo);
+        return getList(boardNo, new Page(1, 10));
+    }
+
+    // 댓글 수정 요청
+    @Transactional
+    public ReplyListResponseDTO modify(final ReplyModifyRequestDTO dto)
+        //DTO 새로 만들고 검증 널기
+        throws SQLException{
+        replyMapper.modify(dto.toEntity());
+
+        return getList(dto.getBno(), new Page(1, 10));
+
+    }
+    // 댓글 수정 서비스
 
 }
